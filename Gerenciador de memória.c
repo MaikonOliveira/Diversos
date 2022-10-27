11cc40902f1e59cbac51e51b7210900b03e8d43f
Maikon Oliveira <115705852+MaikonOliveira@users.noreply.github.com>
2022-10-27 11:30:30 -0300

    Gerenciador de memória (C)

--- Gerenciador de memória.c
+++ Gerenciador de memória.c
@@ -6,8 +6,254 @@
 #include <stdlib.h>
 
 void bold(int status) {
-    static const char *seq[] = {"\x1b[0m", "\x1b[1m"};
+    static const char *seq[] = {"\33[0m", "\33[1m"};
     printf("%s", seq[!!status]);
+}
+
+//Algoritmo que gera o first-fit;
+void first_fit(int reg, int x[], int y[]){
+    int flag=0, flag2=0, aux;
+    for(int i=0; i<10;i++){
+        for(int k=0; k<reg;k++){
+            if(x[k]/y[i] >= 1){
+                x[k]=x[k]-y[i];
+                aux=k;
+                if(x[k]==0) flag2=1;
+                k=reg;
+            }else if(k==(reg-1)){
+                flag=1;
+            }
+        }
+        bold(0);
+        printf("[%d - %d]", i, y[i]);
+        if(flag==0) printf(" [");
+        if(flag==0){
+            for(int j=0;j<reg;j++){
+                if(x[j] == 0);
+                else if(j==0 && aux==0){
+                    bold(1);
+                    printf("%d - ",x[j]);
+                }else if(j==0){
+                    bold(0);
+                    printf("%d - ",x[j]);
+                }else if(j<reg-1 && aux==j){
+                    bold(1);
+                    printf("%d - ",x[j]);
+                }else if(j<reg-1){
+                    bold(0);
+                    printf("%d - ",x[j]);
+                }else if(aux==reg-1){
+                    bold(1);
+                    printf("%d] ",x[j]);
+                    if(flag2==1){
+                        bold(1);
+                        printf("Encaixe perfeito\n");
+                        flag2=0;
+                    }else printf("\n");
+                }else{
+                    bold(0);
+                    printf("%d] ",x[j]);
+                    if(flag2==1){
+                        bold(1);
+                        printf("Encaixe perfeito\n");
+                        flag2=0;
+                    }else printf("\n");
+                }
+            }
+        }else{
+            bold(1);
+            printf(" Solicitacao nao pode ser atendida\n");
+            flag=0;
+        }
+    }
+}
+//Algoritmo que gera o best-fit;
+void best_fit(int reg, int x[], int y[]){
+    int flag=0, flag2=0, aux, valor=100, indice=0;
+    for(int i=0;i<10;i++){
+        for(int k=0;k<reg;k++){
+            if(x[k]-y[i] < valor){
+                if(x[k]-y[i] >= 0){
+                    valor=x[k]-y[i];
+                    indice=k;
+                }
+            }
+            if(k==reg-1 && valor!=100){
+                x[indice]=x[indice]-y[i];
+                aux=indice;
+                if(x[indice]==0) flag2=1;
+                indice=0;
+                valor=100;
+            }else if(k==reg-1) flag=1;
+        }
+        bold(0);
+        printf("[%d - %d]", i, y[i]);
+        if(flag==0) printf(" [");
+        if(flag==0){
+            for(int j=0;j<reg;j++){
+                if(x[j] == 0);
+                else if(j==0 && aux==0){
+                    bold(1);
+                    printf("%d - ",x[j]);
+                }else if(j==0){
+                    bold(0);
+                    printf("%d - ",x[j]);
+                }else if(j<reg-1 && aux==j){
+                    bold(1);
+                    printf("%d - ",x[j]);
+                }else if(j<reg-1){
+                    bold(0);
+                    printf("%d - ",x[j]);
+                }else if(aux==reg-1){
+                    bold(1);
+                    printf("%d] ",x[j]);
+                    if(flag2==1){
+                        bold(1);
+                        printf("Encaixe perfeito\n");
+                        flag2=0;
+                    }else printf("\n");
+                }else{
+                    bold(0);
+                    printf("%d] ",x[j]);
+                    if(flag2==1){
+                        bold(1);
+                        printf("Encaixe perfeito\n");
+                        flag2=0;
+                    }else printf("\n");
+                }
+            }
+        }else{
+            bold(1);
+            printf(" Solicitacao nao pode ser atendida\n");
+            flag=0;
+        }
+    }
+}
+//Algoritmo que gera o worst-fit;
+void worst_fit(int reg, int x[], int y[]){
+    int flag=0, flag2=0, aux, valor=0, indice=0;
+    for(int i=0;i<10;i++){
+        for(int k=0;k<reg;k++){
+            if(x[k]-y[i] > valor){
+                if(x[k]-y[i] >= 0){
+                    valor=x[k]-y[i];
+                    indice=k;
+                }
+            }
+            if(k==reg-1 && valor!=0){
+                x[indice]=x[indice]-y[i];
+                aux=indice;
+                if(x[indice]==0) flag2=1;
+                indice=0;
+                valor=0;
+            }else if(k==reg-1) flag=1;
+        }
+        bold(0);
+        printf("[%d - %d]", i, y[i]);
+        if(flag==0) printf(" [");
+        if(flag==0){
+            for(int j=0;j<reg;j++){
+                if(x[j] == 0);
+                else if(j==0 && aux==0){
+                    bold(1);
+                    printf("%d - ",x[j]);
+                }else if(j==0){
+                    bold(0);
+                    printf("%d - ",x[j]);
+                }else if(j<reg-1 && aux==j){
+                    bold(1);
+                    printf("%d - ",x[j]);
+                }else if(j<reg-1){
+                    bold(0);
+                    printf("%d - ",x[j]);
+                }else if(aux==reg-1){
+                    bold(1);
+                    printf("%d] ",x[j]);
+                    if(flag2==1){
+                        bold(1);
+                        printf("Encaixe perfeito\n");
+                        flag2=0;
+                    }else printf("\n");
+                }else{
+                    bold(0);
+                    printf("%d] ",x[j]);
+                    if(flag2==1){
+                        bold(1);
+                        printf("Encaixe perfeito\n");
+                        flag2=0;
+                    }else printf("\n");
+                }
+            }
+        }else{
+            bold(1);
+            printf(" Solicitacao nao pode ser atendida\n");
+            flag=0;
+        }
+    }
+}
+//Algoritmo que gera o circular-fit ou next-fit;
+void circular_fit(int reg, int x[], int y[]){
+    int flag=0, flag2=0, aux, indice=0, cont=1;
+    for(int i=0; i<10;i++){
+        cont=1;
+        for(int k=indice; k<reg;k++){
+            if(x[k]/y[i] >= 1){
+                x[k]=x[k]-y[i];
+                indice=k+1;
+                if(indice==reg) indice=0;
+                aux=k;
+                if(x[k]==0) flag2=1;
+                k=reg;
+            }else if(cont==reg){
+                flag=1;
+                k=reg;
+            }else if(k==(reg-1)){
+                k=0;
+            }
+            cont++;
+        }
+        bold(0);
+        printf("[%d - %d]", i, y[i]);
+        if(flag==0) printf(" [");
+        if(flag==0){
+            for(int j=0;j<reg;j++){
+                if(x[j] == 0);
+                else if(j==0 && aux==0){
+                    bold(1);
+                    printf("%d - ",x[j]);
+                }else if(j==0){
+                    bold(0);
+                    printf("%d - ",x[j]);
+                }else if(j<reg-1 && aux==j){
+                    bold(1);
+                    printf("%d - ",x[j]);
+                }else if(j<reg-1){
+                    bold(0);
+                    printf("%d - ",x[j]);
+                }else if(aux==reg-1){
+                    bold(1);
+                    printf("%d] ",x[j]);
+                    if(flag2==1){
+                        bold(1);
+                        printf("Encaixe perfeito\n");
+                        flag2=0;
+                    }else printf("\n");
+                }else{
+                    bold(0);
+                    printf("%d] ",x[j]);
+                    if(flag2==1){
+                        bold(1);
+                        printf("Encaixe perfeito\n");
+                        flag2=0;
+                    }else printf("\n");
+                }
+            }
+        }else{
+            bold(1);
+            printf(" Solicitacao nao pode ser atendida\n");
+            flag=0;
+        }
+    }
 }
 
 int main(){
@@ -19,13 +265,27 @@ int main(){
         printf(" 0 - First-fit\n 1 - Best-fit\n 2 - Worst-fit\n 3 - Circular-fit\n");
         scanf("%d", &OP);
     }while(OP!=0 && OP!=1 && OP!=2 && OP!=3);
+    switch(OP){
+        case 0:
+            printf("===== Algoritmo First-fit! =====\n");
+            break;
+        case 1:
+            printf("===== Algoritmo Best-fit! =====\n");
+            break;
+        case 2:
+            printf("===== Algoritmo Worst-fit! =====\n");
+            break;
+        case 3:
+            printf("===== Algoritmo Circular-fit! =====\n");
+            break;
+    }
     srand((unsigned)time(NULL) );
     T=8+rand()%(15-8+1);
     int vet[T];
-    printf("Regioes: %d\n",T);
+    //printf("Regioes: %d\n",T);
     //Gera um valor aleatório para cada região e imprime no terminal as regiões originais.
     for(int i=0;i<T;i++){
-        vet[i]=1+rand()%(100);
+        vet[i]=1+rand()%(99);
     }for(int i=0;i<T;i++){
         if(i==0) printf("Original [%d - ",vet[i]);
         else if(i<T-1) printf("%d - ",vet[i]);
@@ -53,242 +313,5 @@ int main(){
             circular_fit(T, vet, vet2);
             printf("\n");
             break;
-    }
-}
-//Algoritmo que gera o first-fit;
-void first_fit(int reg, int x[], int y[]){
-    int flag=0, flag2=0, aux;
-    for(int i=0; i<10;i++){
-        for(int k=0; k<reg;k++){
-            if(x[k]/y[i] >= 1){
-                x[k]=x[k]-y[i];
-                aux=k;
-                if(x[k]==0) flag2=1;
-                k=reg;
-            }else if(k==(reg-1)){
-                flag=1;
-            }
-        }
-        printf("[%d - %d]", i, y[i]);
-        if(flag==0){
-            for(int j=0;j<reg;j++){
-                if(x[j] == 0);
-                else if(j==0 && aux==0){
-                    bold(2);
-                    printf(" [%d - ",x[j]);
-                }else if(j==0){
-                    bold(0);
-                    printf(" [%d - ",x[j]);
-                }else if(j<reg-1 && aux==j){
-                    bold(2);
-                    printf("%d - ",x[j]);
-                }else if(j<reg-1){
-                    bold(0);
-                    printf("%d - ",x[j]);
-                }else if(aux==reg-1){
-                    bold(2);
-                    printf("%d] ",x[j]);
-                    if(flag2==1){
-                        bold(1);
-                        printf("Encaixe perfeito\n");
-                        flag2=0;
-                    }else printf("\n");
-                }else{
-                    bold(0);
-                    printf("%d] ",x[j]);
-                    if(flag2==1){
-                        bold(2);
-                        printf("Encaixe perfeito\n");
-                        flag2=0;
-                    }else printf("\n");
-                }
-            }
-        }else{
-            bold(2);
-            printf(" Solicitacao nao pode ser atendida\n");
-            flag=0;
-        }
-    }
-}
-//Algoritmo que gera o best-fit;
-void best_fit(int reg, int x[], int y[]){
-    int flag=0, flag2=0, aux, valor=100, indice=0;
-    for(int i=0;i<10;i++){
-        for(int k=0;k<reg;k++){
-            if(x[k]-y[i] < valor){
-                if(x[k]-y[i] >= 0){
-                    valor=x[k]-y[i];
-                    indice=k;
-                }
-            }
-            if(k==reg-1 && valor!=100){
-                x[indice]=x[indice]-y[i];
-                aux=indice;
-                if(x[indice]==0) flag2=1;
-                indice=0;
-                valor=100;
-            }else if(k==reg-1) flag=1;
-        }
-        printf("[%d - %d]", i, y[i]);
-        if(flag==0){
-            for(int j=0;j<reg;j++){
-                if(x[j] == 0);
-                else if(j==0 && aux==0){
-                    bold(2);
-                    printf(" [%d - ",x[j]);
-                }else if(j==0){
-                    bold(0);
-                    printf(" [%d - ",x[j]);
-                }else if(j<reg-1 && aux==j){
-                    bold(2);
-                    printf("%d - ",x[j]);
-                }else if(j<reg-1){
-                    bold(0);
-                    printf("%d - ",x[j]);
-                }else if(aux==reg-1){
-                    bold(2);
-                    printf("%d] ",x[j]);
-                    if(flag2==1){
-                        bold(1);
-                        printf("Encaixe perfeito\n");
-                        flag2=0;
-                    }else printf("\n");
-                }else{
-                    bold(0);
-                    printf("%d] ",x[j]);
-                    if(flag2==1){
-                        bold(2);
-                        printf("Encaixe perfeito\n");
-                        flag2=0;
-                    }else printf("\n");
-                }
-            }
-        }else{
-            bold(2);
-            printf(" Solicitacao nao pode ser atendida\n");
-            flag=0;
-        }
-    }
-}
-//Algoritmo que gera o worst-fit;
-void worst_fit(int reg, int x[], int y[]){
-    int flag=0, flag2=0, aux, valor=0, indice=0;
-    for(int i=0;i<10;i++){
-        for(int k=0;k<reg;k++){
-            if(x[k]-y[i] > valor){
-                if(x[k]-y[i] >= 0){
-                    valor=x[k]-y[i];
-                    indice=k;
-                }
-            }
-            if(k==reg-1 && valor!=0){
-                x[indice]=x[indice]-y[i];
-                aux=indice;
-                if(x[indice]==0) flag2=1;
-                indice=0;
-                valor=0;
-            }else if(k==reg-1) flag=1;
-        }
-        printf("[%d - %d]", i, y[i]);
-        if(flag==0){
-            for(int j=0;j<reg;j++){
-                if(x[j] == 0);
-                else if(j==0 && aux==0){
-                    bold(2);
-                    printf(" [%d - ",x[j]);
-                }else if(j==0){
-                    bold(0);
-                    printf(" [%d - ",x[j]);
-                }else if(j<reg-1 && aux==j){
-                    bold(2);
-                    printf("%d - ",x[j]);
-                }else if(j<reg-1){
-                    bold(0);
-                    printf("%d - ",x[j]);
-                }else if(aux==reg-1){
-                    bold(2);
-                    printf("%d] ",x[j]);
-                    if(flag2==1){
-                        bold(1);
-                        printf("Encaixe perfeito\n");
-                        flag2=0;
-                    }else printf("\n");
-                }else{
-                    bold(0);
-                    printf("%d] ",x[j]);
-                    if(flag2==1){
-                        bold(2);
-                        printf("Encaixe perfeito\n");
-                        flag2=0;
-                    }else printf("\n");
-                }
-            }
-        }else{
-            bold(2);
-            printf(" Solicitacao nao pode ser atendida\n");
-            flag=0;
-        }
-    }
-}
-//Algoritmo que gera o circular-fit ou next-fit;
-void circular_fit(int reg, int x[], int y[]){
-    int flag=0, flag2=0, aux, indice=0, cont=1;
-    for(int i=0; i<10;i++){
-        cont=1;
-        for(int k=indice; k<reg;k++){
-            if(x[k]/y[i] >= 1){
-                x[k]=x[k]-y[i];
-                indice=k+1;
-                if(indice==reg) indice=0;
-                aux=k;
-                if(x[k]==0) flag2=1;
-                k=reg;
-            }else if(cont==reg){
-                flag=1;
-                k=reg;
-            }else if(k==(reg-1)){
-                k=0; 
-            }
-            cont++;
-        }
-        printf("[%d - %d]", i, y[i]);
-        if(flag==0){
-            for(int j=0;j<reg;j++){
-                if(x[j] == 0);
-                else if(j==0 && aux==0){
-                    bold(2);
-                    printf(" [%d - ",x[j]);
-                }else if(j==0){
-                    bold(0);
-                    printf(" [%d - ",x[j]);
-                }else if(j<reg-1 && aux==j){
-                    bold(2);
-                    printf("%d - ",x[j]);
-                }else if(j<reg-1){
-                    bold(0);
-                    printf("%d - ",x[j]);
-                }else if(aux==reg-1){
-                    bold(2);
-                    printf("%d] ",x[j]);
-                    if(flag2==1){
-                        bold(1);
-                        printf("Encaixe perfeito\n");
-                        flag2=0;
-                    }else printf("\n");
-                }else{
-                    bold(0);
-                    printf("%d] ",x[j]);
-                    if(flag2==1){
-                        bold(2);
-                        printf("Encaixe perfeito\n");
-                        flag2=0;
-                    }else printf("\n");
-                }
-            }
-        }else{
-            bold(2);
-            printf(" Solicitacao nao pode ser atendida\n");
-            flag=0;
-        }
     }
 }
